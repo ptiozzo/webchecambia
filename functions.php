e681f542348e95074a4feb1c90a808ed5e642580
Paolo <paolo@EDP4YOUs-MacBook-Air.local>
2020-02-18 17:15:22 +0100

    Completamento file fondamentali

--- functions.php
+++ functions.php
@@ -36,10 +36,50 @@ if ( ! function_exists( 'wcc_styles_scripts' ) ) {
 		wp_enqueue_script( 'wcc-swupScriptsPlugin-js', get_template_directory_uri().'/assets/swup/SwupScriptsPlugin.min.js','','',true);
 		wp_enqueue_script( 'wcc-swup-custom-js', get_template_directory_uri().'/assets/swup/script.js','','',true);
 		wp_enqueue_script( 'wcc-fontawsome-js', '//kit.fontawesome.com/befb91387f.js','','',true);
-
 	}
 }
 add_action( 'wp_enqueue_scripts', 'wcc_style_scripts' );
+
+/*  Theme setup
+/* ------------------------------------ */
+if ( ! function_exists( 'wcc_setup' ) ) {
+	function wcc_setup() {
+		add_theme_support( "title-tag" );
+		// Enable automatic feed links
+		add_theme_support( 'automatic-feed-links' );
+		// Enable featured image
+		add_theme_support( 'post-thumbnails' );
+
+		// Custom menu areas
+		register_nav_menus( array(
+			'menu-principale' => esc_html__( 'Menu principale', 'wcc' ),
+			'menu-social' => esc_html__( 'Menu social', 'wcc' ),
+			'menu-footer' => esc_html__( 'Menu footer', 'wcc' ),
+		) );
+	}
+}
+add_action( 'after_setup_theme', 'wcc_setup' );
+require_once('assets/bs4navwalker.php');
+
+
+if ( ! function_exists( 'wcc_sidebars' ) ) {
+	function wcc_sidebars()	{
+		register_sidebar(array( 'name' => esc_html__( 'Primary', 'wcc' ),'id' => 'primary','description' => esc_html__( 'Sidebar homepage non visualizzata su mobile', 'wcc' ), 'before_widget' => '<div id="%1$s" class="widget %2$s">','after_widget' => '</div>','before_title' => '<h3>','after_title' => '</h3>'));
+	}
+}
+add_action( 'widgets_init', 'wcc_sidebars' );
+
+/*
+* Add Response code to video embeds in WordPress
+*
+* @refer  http://alxmedia.se/code/2013/10/make-wordpress-default-video-embeds-responsive/
+*/
+function abl1035_alx_embed_html( $html ) {
+
+ return '<div class="video-container">' . $html . '</div>';
+}
+add_filter( 'embed_oembed_html', 'abl1035_alx_embed_html', 10, 3 );
+add_filter( 'video_embed_html', 'abl1035_alx_embed_html' ); // Jetpack
 
 
 
